a64bcf23cb4c7cf8b1b8b84ea121ee313dcfd9af
Wilhelm Schuster <dev@rot13.io>
2024-08-15 22:16:46 +0200

    firmware: Migrate bootmagic lite to info.json

--- firmware/keyboards/leopold/fc980c/wlhlm/config.h
+++ firmware/keyboards/leopold/fc980c/wlhlm/config.h
@@ -3,7 +3,6 @@
 
 #pragma once
 
-
 /* Custom matrix pins and port select array */
 #define MATRIX_KEY_ENABLE_PIN A4
 #define MATRIX_KEY_STATE_PIN B15
@@ -14,10 +13,5 @@
 /* While the original BOM uses an M24C64 EEPROM not directly supported by QMK,
  * said EEPROM uses the same parameters as the already supported 24LC64. */
 #define EEPROM_I2C_24LC64
-
-/* Bootmagic Lite key configuration */
-// Esc key, i.e. the first key in the top row.
-#define BOOTMAGIC_LITE_ROW 0
-#define BOOTMAGIC_LITE_COLUMN 13
 
 //#define DEBUG_MATRIX_SCAN_RATE
